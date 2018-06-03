7e90e0cddd5dacdca5817707482ec48dc3c486f9
Jaranchai <jaranchai.nt@hotmail.com>
2018-06-03 16:26:04 +0700

    warning eslint
    - fix warning from eslint
    - change title in index.html
    - remove port

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { addLocalStorage, getLocalStorage } from '../../actions/example'
+import { getLocalStorage } from '../../actions/example'
 
 class Index extends Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'whatwg-fetch'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import localStorage from 'localStorage'
+// import localStorage from 'localStorage'
 
 import registerServiceWorker from './registerServiceWorker'
 
